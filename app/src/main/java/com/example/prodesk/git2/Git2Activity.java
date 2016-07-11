c9755ebb1ace395eb07b6df1a7712f9dee102825
Git <steven1203@naver.com>
2016-07-11 20:20:07 +0900

    commit test

---
+++ app/src/main/java/com/example/prodesk/git2/Git2Activity.java
@@ -0,0 +1,14 @@
+package com.example.prodesk.git2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Git2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git2);
+
+    }
+}
